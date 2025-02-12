67264711e48c6f6b43404703982cafbfd007cc43
ZetGo <73279716+ZetGoHack@users.noreply.github.com>
2025-02-13 01:08:24 +0200

    test optimisation #8
    dump

--- VidDownloader.py
+++ VidDownloader.py
@@ -108,9 +108,9 @@ class VidDownloaderMod(loader.Module):
                 width, height = map(int, resolution.split('x'))
             
             if os.path.exists(filename):
-                await inlmessage.edit(text=f"☑️ {'Видео скачано' if humanfrmt != 'mp3' else 'Музыка скачана'}, жди {'окончания обработки и' if humanfrmt == 'mp3' else ''} выгрузки!")
+                await inlmessage.edit(text=f"☑️ {'Видео скачано' if self.chsn != 'mp3' else 'Музыка скачана'}, жди {'окончания обработки и' if humanfrmt == 'mp3' else ''} выгрузки!")
                 try:
-                    if humanfrmt != 'mp3':
+                    if self.chsn != 'mp3':
                         await self.client.send_message(
                             utils.get_chat_id(self.message),
                             filename, force_document=False,
